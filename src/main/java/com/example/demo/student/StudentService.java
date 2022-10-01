4ec0ca429076417ee04ee38ebfe2556a04983e52
Balazs2014 <tbalazs.levente@gmail.com>
2022-10-01 18:20:11 +0200

    GET, POST, DELETE metódusok

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void postStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Foglalt email");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Nem létezik");
+        }
+        studentRepository.deleteById(studentId);
+    }
+}
